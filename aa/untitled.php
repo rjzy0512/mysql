bdd728ccb0913f52325a2b24072f09e77e477698
入江 <2237589262@qq.com>
2018-10-19 10:33:29 +0800

    mysql
    增加更新

--- aa/untitled.php
+++ aa/untitled.php
@@ -13,13 +13,17 @@ $link= mysql_connect("localhost","root","root") or die("数据连接失败");
    else                                                                                
     echo"链接失败";*/
 	mysql_query ("set names utf8");
-	$select=mysql_select_db("itcast")or die("数据库访问失败".mysql_error());
+	$select=mysql_select_db("itcst")or die("数据库访问失败".mysql_error());
 	$sql="select * from emp_info";
 	$result=mysql_query($sql,$link);
-	while($row=mysql_fetch_row($result))
+	/*while($row=mysql_fetch_row($result))
 					$emp_info[]=$row;
 					foreach($emp_info as $v)
-						echo $v[0],'',$v[1],'',$v[2],'',$v[3],'',$v[4],'',"<br>";
+						echo $v[0],'',$v[1],'',$v[2],'',$v[3],'',$v[4],'',"<br>";*/
+							while($row=mysql_fetch_assoc($result))
+					$emp_info[]=$row;
+					foreach($emp_info as $v)
+						echo $v["e_id"],'',$v["e_name"],'',$v["e_dapt"],'',$v["date_of_birth"],'',$v["date_of_entry"],'',"<br>";
 
 ?>
 </body>
